14042d0881b3088e4cd659dd388c579ee1b25074
jose <jmadrigal_94@outlook.com>
2021-04-26 07:23:45 -0600

    Se agrega una prueba mas a las pruebas unitarias

--- src/test/java/PruebasIntegracion/TestPruebasIntegracion.java
+++ src/test/java/PruebasIntegracion/TestPruebasIntegracion.java
@@ -28,26 +28,26 @@ public class TestPruebasIntegracion extends BaseTest {
         pruebasIntegracion.FilterByArticle();
     }
 
-    @Parameters({"NameArt1", "URL"})
+    @Parameters({"nameArt1", "URL"})
     @Test(priority = 5)
     public void MethodSetUpFilter1(String word, String URL) throws InterruptedException {
         pruebasIntegracion.FilterArticle(word);
         driver.navigate().to(URL);
     }
 
-    @Parameters({"NameArt2", "URL"})
+    @Parameters({"nameArt2", "URL"})
     @Test(priority = 6)
     public void MethodSetUpFilter2(String word, String URL) throws InterruptedException {
         pruebasIntegracion.FilterArticle(word);
     }
 
-    @Parameters({"NameArt3", "URL"})
+    @Parameters({"nameArt3", "URL"})
     @Test(priority = 7)
     public void MethodSetUpFilter3(String word, String URL) throws InterruptedException {
         pruebasIntegracion.FilterArticle(word);
     }
 
-    @Parameters({"NameArt4", "URL"})
+    @Parameters({"nameArt4", "URL"})
     @Test(priority = 8)
     public void MethodSetUpFilter4(String word, String URL) throws InterruptedException {
         pruebasIntegracion.FilterArticle(word);
--- src/test/java/PruebasUnitarias/TestPruebasUnitarias.java
+++ src/test/java/PruebasUnitarias/TestPruebasUnitarias.java
@@ -6,23 +6,30 @@ import org.testng.annotations.Test;
 
 public class TestPruebasUnitarias extends BaseTest {
 
-    @Parameters({"Name", "emailNew", "passwordNew", "passwordConfirm", "URL"})
+    @Parameters({"name", "emailNew", "passwordNew", "passwordConfirm", "URL"})
     @Test(priority = 1)
     public void CreateAccount(String Name, String emailNew, String passwordNew, String passwordConfirm, String URL) {
         pruebasUnitarias.CreateAccount(Name, emailNew, passwordNew, passwordConfirm);
         driver.navigate().to(URL);
     }
 
-    @Parameters({"NameFail", "emailNewFail", "passwordNewFail", "passwordConfirmFail", "URL"})
+    @Parameters({"nameFail", "emailNewFail", "passwordNewFail", "passwordConfirmFail", "URL"})
     @Test(priority = 2)
     public void CreateAccountFail(String Name, String emailNew, String passwordNew, String passwordConfirm, String URL) {
         pruebasUnitarias.CreateAccountFail(Name, emailNew, passwordNew, passwordConfirm);
         driver.navigate().to(URL);
     }
 
-    @Parameters({"emailFail", "loginFailText"})
+    @Parameters({"emailFail", "loginFailText", "URL"})
     @Test(priority = 3)
-    public void LoginFail(String email, String loginFailText) {
+    public void LoginFail(String email, String loginFailText, String URL) {
         pruebasUnitarias.LoginFail(email, loginFailText, "//span[@class='a-list-item']");
+        driver.navigate().to(URL);
+    }
+
+    @Parameters({"invalidEmailFormat", "loginFailText"})
+    @Test(priority = 4)
+    public void LoginFailWithInvalidEmailFormat(String invalidEmailFormat, String loginFailText) {
+        pruebasUnitarias.LoginFail(invalidEmailFormat, loginFailText, "//span[@class='a-list-item']");
     }
 }
